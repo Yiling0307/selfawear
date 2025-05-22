c12dab3dc392e734e466020ca35c930a1955267e
Yiling0307 <yiling.chen7@student.unimelb.edu.au>
2025-05-22 13:49:21 +0800

    index.html complete

---
+++ js/header.js
@@ -0,0 +1,17 @@
+document.querySelector('header .content').innerHTML = `
+  <img src="./assets/logo.png" width="145px" style="margin-right: 41px;" alt="">
+  <div class="menu">NEW RELEASES</div>
+  <div class="menu">GROUNDING</div>
+  <div class="menu">ACTIVEWEAR</div>
+  <div class="menu">CLOTHING</div>
+  <div class="menu">JEWELLERY</div>
+  <div class="menu">ACCESSORIES</div>
+  <div class="menu">MISSION</div>
+  <div class="menu pink">SALE</div>
+
+  <div class="user-info">
+    <img src="./assets/search-normal.png" width="24px" alt="">
+    <img src="./assets/car.png" width="24px" alt="">
+    <img src="./assets/avatar.png" width="24px" alt="">
+  </div>
+`
