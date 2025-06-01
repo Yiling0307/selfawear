1878733b1148c2e99bb629df49a3bae0a71a5e5f
Yiling0307 <yiling.chen7@student.unimelb.edu.au>
2025-06-02 00:38:02 +0800

    mobile bag

--- mobile/js/header.js
+++ mobile/js/header.js
@@ -25,3 +25,53 @@ function toPCPage() {
   const currentPage = pathArray[pathArray.length - 1];
   window.location.href = `/${currentPage}`;
 }
+
+let bagPanel = `
+  <div class="bag-mask" onclick="hideBagPanel()"></div>
+  <div class="bag-panel">
+    <div class="product-list">
+      <div class="product-item">
+        <img src="./assets/bag-cover.png" width="130px" alt="">
+        <div>
+          <div style="font-weight: bold">Spread Kindness Pocket T-Shirt Black</div>
+          <div class="size">Size: S</div>
+          <div class="num-wrap">
+            <div class="num-input">
+              <div>-</div>
+              <input type="text" value="1">
+              <div>+</div>
+            </div>
+            <div style="font-weight: bold">$147.00</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="total-price">
+      <span>SUBTOTAL</span>
+      <span>$147.00</span>
+    </div>
+    <div class="tips">Shipping, taxes, and discount codes calculated at checkout.</div>
+    <div class="check-button flex-center" onclick="toPayment()">Check out</div>
+
+  </div>
+`
+// Insert bagPanel into the body
+const bodyElement = document.querySelector('body');
+const bagPanelElement = document.createElement('div');
+bagPanelElement.innerHTML = bagPanel;
+bodyElement.appendChild(bagPanelElement);
+
+function showBagPanel() {
+  document.querySelector('.bag-panel').classList.add('open')
+  document.querySelector('.bag-mask').style.display = 'block'
+}
+
+function hideBagPanel() {
+  document.querySelector('.bag-panel').classList.remove('open')
+  document.querySelector('.bag-mask').style.display = 'none'
+}
+
+function toPayment() {
+  window.location.href = '/payment.html'
+}
